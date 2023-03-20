535809971da2b6d6e601328c155143b069b47984
Junaid Me <junaidjunni953@gmail.com>
2023-03-20 12:40:32 +0530

    Assignment encaps

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+
+
+        obj.setName("junaid");
+
+        System.out.println(obj.getName());
+
+
+
+
+
+    }
 }
